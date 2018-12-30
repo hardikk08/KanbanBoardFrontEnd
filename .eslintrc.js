2804ba10df3f94ff7a862e0c8926d46c5d65d7bc
Hardik Nagda <hardiksmac@Hardiks-MacBook-Air.local>
2018-12-30 17:53:33 +0530

    Initialize project

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,11 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'brace-style': [2, 'stroustrup', { 'allowSingleLine': true }],
+    'indent': 'off',
+    'no-tabs': 'off',
+    'no-unused-expressions': 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
